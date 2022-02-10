7626833d8dde733fdb495a17612809bad395e3ba
Prithvi Kannan <46332835+prithvikannan@users.noreply.github.com>
2022-02-09 23:19:43 -0800

    skeleton pages and app routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import HomePage from "./Pages/HomePage";
+import LandingPage from "./Pages/LandingPage";
+import LoginPage from "./Pages/LoginPage";
+import SignupPage from "./Pages/SignupPage";
+import BrowsePage from "./Pages/BrowsePage";
+import ReservePage from "./Pages/ReservePage";
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="#/">DineSmart</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#/home">Home</Nav.Link>
+            <Nav.Link href="#/login">Login</Nav.Link>
+            <Nav.Link href="#/browse">Browse</Nav.Link>
+            <Nav.Link href="#/reserve">Reserve</Nav.Link>
+          </Nav>
+          {/* <Navbar.Collapse className="justify-content-end">
+            <TryUsername user={this.state.user}></TryUsername>
+            <Navbar.Text onClick={this.onSubmit}>
+              <a href="#login">Logout</a>
+            </Navbar.Text>
+          </Navbar.Collapse> */}
+        </Navbar>
+        <HashRouter>
+          <div className="App">
+            <Routes>
+              <Route exact path="/" element={<LandingPage/>} />
+              <Route path="/home" element={<HomePage />} />
+              <Route
+                path="/login"
+                render={() => <LoginPage handler={this.handleClick} />}
+              />
+              <Route
+                path="/signup"
+                render={() => <SignupPage handler={this.handleClick} />}
+              />
+              <Route path="/browse" element={<BrowsePage/>} />
+              <Route path="/reserve/" element={<ReservePage/>} />
+            </Routes>
+          </div>
+        </HashRouter>
+      </Container>
+    );
+  }
 }
 
 export default App;
