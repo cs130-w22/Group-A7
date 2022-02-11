76c4fb28ec90f37701a096497f39a2d8264077a7
Prithvi Kannan <46332835+prithvikannan@users.noreply.github.com>
2022-02-11 09:54:25 -0800

    create a login and signup page layout

--- frontend/src/Pages/LoginPage.js
+++ frontend/src/Pages/LoginPage.js
@@ -1,16 +1,59 @@
 import React, { Component } from "react";
+import {  NavLink } from "react-router-dom";
+import { Row, Col, Container, Form, Button } from "react-bootstrap";
 
-export default class HomePage extends Component {
+export default class LoginPage extends Component {
+  state = {
+    email: "",
+    password: "",
+    loggedIn: false,
+  };
+
   render() {
     return (
-      <div className="Home">
-        <header className="Home-header">
-          <p>
-            This is the login page
-          </p>
-        </header>
-      </div>
-    )
+      <Container>
+        <h1>Login Page</h1>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={this.state.email}
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={(e) => this.setState({ password: e.target.value })}
+            />
+          </Form.Group>
+          <Col>
+            <Row>
+              <NavLink to="/home/" onClick={this.onSubmit}>
+                Submit
+              </NavLink>
+            </Row>
+            <Row>
+              <Button href="#/signup/" variant="outline-secondary">
+                Don't have an account? Make one here.
+              </Button>
+            </Row>
+            <Row>
+              <NavLink to="/" onClick={this.passWordReset}>
+                Request password reset
+              </NavLink>
+            </Row>
+          </Col>
+        </Form>
+      </Container>
+    );
   }
 }
-
