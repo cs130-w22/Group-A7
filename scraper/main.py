166aa4d7015c3749f8518cd2b5b027fba0d71bc0
vsathye <49597649+vsathye@users.noreply.github.com>
2022-02-11 01:05:06 -0800

    Create main.py

---
+++ scraper/main.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://google.com"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id="PUTIDNAMEHERE")
+
+print(results.prettify())
