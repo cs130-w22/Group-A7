245dfc1ad88b2998a12bb740618946d7c8fa2e6c
vsathye <49597649+vsathye@users.noreply.github.com>
2022-02-11 20:24:58 -0800

    inital table schema

--- scraper/main.py
+++ scraper/main.py
@@ -1,11 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://google.com"
+
+city = "ny"
+date = "2022-02-11"
+seats = "2"
+query = "mexican"
+URL = f"https://resy.com/cities/{city}?date={date}&seats={seats}&query={query}"
+
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-results = soup.find(id="PUTIDNAMEHERE")
+results = soup.find(id="ScrollTo")
 
 print(results.prettify())
