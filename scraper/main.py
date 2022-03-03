6cb1ed0f4dfe0c98cba810881a7259ce039f10f7
vsathye <49597649+vsathye@users.noreply.github.com>
2022-03-03 12:54:08 -0800

    Update main.py

--- scraper/main.py
+++ scraper/main.py
@@ -1,17 +1,45 @@
-import requests
+# import requests
+# from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+# from selenium import webdriver
+# import selenium
+# import http.client as httplib
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.common.exceptions import TimeoutException
+# from selenium.common.exceptions import NoSuchElementException
+# from selenium.common.exceptions import StaleElementReferenceException
+# from selenium.common.exceptions import WebDriverException
+# from datetime import datetime as dt
 from bs4 import BeautifulSoup
 
-
 city = "ny"
-date = "2022-02-11"
+date = "2022-03-03"
 seats = "2"
 query = "mexican"
 URL = f"https://resy.com/cities/{city}?date={date}&seats={seats}&query={query}"
+# print(URL)
 
-page = requests.get(URL)
+# page = requests.get(URL)
+# print(page.content)
+# soup = BeautifulSoup(page.content, "html.parser")
+# divs = soup.findAll(class_= "ScrollTo")
+# print(divs)
 
-soup = BeautifulSoup(page.content, "html.parser")
+session = HTMLSession()
+r = session.get(URL)
+print(r.full_text)
 
-results = soup.find(id="ScrollTo")
+# browser = webdriver.Chrome(executable_path='/Users/vedantsathye/Documents/chromedriver')
+# browser.delete_all_cookies()
+# browser.get(URL)
 
-print(results.prettify())
+# nframe = browser.find_element_by_class_name('ScrolTo')
+# browser.switch_to_frame(nframe)
+
+# c = browser.page_source
+# soup = BeautifulSoup(c, "html.parser")
+
+# all = soup.find_all("div", {"class": "ScrollTo"})
+# print(all)
