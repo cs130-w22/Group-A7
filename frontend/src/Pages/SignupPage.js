657d913444ed03b0fc0411313c48ae762a351bd1
Prithvi Kannan <46332835+prithvikannan@users.noreply.github.com>
2022-03-04 09:41:14 -0800

    mocked API for signup

--- frontend/src/Pages/SignupPage.js
+++ frontend/src/Pages/SignupPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Row, Col, Container, Form, Button } from "react-bootstrap";
+import { Navigate } from "react-router-dom";
 
 export default class SignupPage extends Component {
   state = {
@@ -10,6 +11,68 @@ export default class SignupPage extends Component {
     loggedIn: false,
   };
 
+  componentDidMount() {
+    // TODO: uncomment once API is live
+    // axios({
+    //   method: "post",
+    //   url: "getCurrentUser/",
+    //   headers: {
+    //     "Content-Type": "text/plain",
+    //     "X-CSRFToken": Cookies.get("XSRF-TOKEN"),
+    //   },
+    //   withCredentials: true,
+    // })
+    //   .then((response) => {
+    //     if (response.data !== "") {
+    //       this.setState({ loggedIn: true });
+    //     }
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+  }  
+
+  isValid = () => {
+    var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+    if (this.state.password.match(passw)) {
+      return true;
+    }
+    alert("Password not good enough");
+    return false;
+  };
+
+  onSubmit = () => {
+    if (!this.isValid()) {
+      return;
+    }
+    var data = JSON.stringify({
+      email: this.state.email,
+      password: this.state.password,
+      role: this.state.role,
+    });
+    // TODO: uncomment once API is live
+    // var config = {
+    //   method: "post",
+    //   url: "createUser/",
+    //   headers: {
+    //     "Content-Type": "text/plain",
+    //     "X-CSRFToken": Cookies.get("XSRF-TOKEN"),
+    //   },
+    //   data: data,
+    //   withCredentials: true,
+    // };
+
+    // axios(config)
+    //   .then((response) => {
+    //     console.log(response.status);
+    //     console.log(JSON.stringify(response.data));
+    //     this.props.handler();
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+    return <Navigate to="/home" />;
+  };
   render() {
     return (
       <Container>
