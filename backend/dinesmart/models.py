f4d320e0f37660619da7f469e46587cb5fdd9756
Aayush Patel <patelaayush00@gmail.com>
2022-03-07 11:52:12 -0800

    Add customer reviews database model

--- backend/dinesmart/models.py
+++ backend/dinesmart/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Users(models.Model):
@@ -14,3 +15,10 @@ class PasswordReset(models.Model):
     email = models.CharField(max_length=50)
     resetToken = models.CharField(max_length=50)
     timestamp = models.CharField(max_length=50)
+
+class Reviews(models.Model):
+    user = models.CharField(max_length=50)
+    restaurant = models.CharField(max_length=50)
+    rating = models.IntegerField(validators=[MinValueValidator(0),
+                                       MaxValueValidator(5)])
+    content = models.CharField(max_length=500)
