acffceb7c5c34abd57c1ad7c212bc9229da59ace
Faraz Murshed <farazmurshed@gmail.com>
2022-03-07 12:28:13 -0800

    scrape exploretock instead of resy

--- backend/scraper/main.py
+++ backend/scraper/main.py
@@ -1,79 +1,39 @@
-import re
-
-# test input
-city = "la"
-date = "2022-03-03"
-seats = "2"
-query = "american"
-URL = f"https://resy.com/cities/{city}?date={date}&seats={seats}&query={query}"
-
-from requests_html import AsyncHTMLSession
-
-asession = AsyncHTMLSession()
-
-# scrape Resy for restaurant results
-async def get_results():
-    global city 
-    global date 
-    global seats 
-    global query 
-    global URL 
-    r = await asession.get(URL)
-    await r.html.arender(wait = 3, sleep = 3)
-    return r
-
-# parse restaurant results to get restaurant names
-def extract_resy_names(results):
-    resy_lines = results.splitlines()
-    resy_names = []
-    for i in range(len(resy_lines)):
-        revRegex = re.compile(r'\$')
-        res = revRegex.search(resy_lines[i])
-        if res is not None:
-            resy_names.append(resy_lines[i - 5])
-    return resy_names
+import requests
+from bs4 import BeautifulSoup
 
 
+def get_restaurant_info(city_hyphened, city_spaced, date, size):
+    url = f"https://www.exploretock.com/city/{c1}/search?city={c2}&date={date}&size={size}"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
 
-def cleanser(places, text):
-    delimeters = '|'.join(places)
-    m_dict = {}
+    restaurants = dict()
+    search_block = soup.find('div',
+                             class_='AvailabilitySearch-destinationResultList AvailabilitySearch-destinationResultList--simple')
+    if search_block is None:
+        return {}
+    search_list = search_block.findAll('li', attrs={'data-businessid': True})
+    if search_list is None:
+        return {}
 
-    for r in places:
-        for i in re.split(delimeters, text):
-            if 'PM' in i or 'AM' in i:
-                for line in i.split('\n'):
-                    if 'PM' in line or 'AM' in line:
-                        if r in m_dict:
-                            m_dict[r].append(line)
-                        else:
-                            m_dict[r] = [line]
-        m_dict[r] = list(set(m_dict[r]))
-    return m_dict
+    for i in range(len(search_list)):
+        name_slot = \
+        soup.findAll('h2', class_='MuiTypography-root jss5 jss31 jss21 css-0 MuiTypography-body1 MuiTypography-noWrap')[
+            i]
+        name = name_slot.getText()
 
-def get_scraper_info(c,d,s,q):
-    # scrape resy and get 20 restaurant names
-    global city 
-    global date 
-    global seats 
-    global query 
-    global URL 
-    city = c
-    date = d
-    seats = s
-    query = q
-    URL = f"https://resy.com/cities/{city}?date={date}&seats={seats}&query={query}"
-    r = asession.run(get_results)
-    latter = r[0].html.text.split("Guests")
-    former = latter[-1].split("‹")[0]
-    names = extract_resy_names(former)
+        times = []
+        for sl in search_list[i]:
+            time_slots = sl.findAll('button', attrs={'data-testid': 'select-time'})
+            for ts in time_slots:
+                times.append(ts.getText())
 
-    return cleanser(names, former)
+        restaurants[name] = times
+    return restaurants
 
-print(get_scraper_info("ny", "2022-03-07", "2", "mexican"))
-
-
-
-
-
-
+# test
+#c1 = "los-angeles"
+#c2 = "Los Angeles"
+#date = "2022-03-07"
+#size = "2"
+#print(get_restaurant_info(c1, c2, date, size))
