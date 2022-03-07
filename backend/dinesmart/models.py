9b13382feaf1771e795ae96ac4808e1ad2962284
Aayush Patel <patelaayush00@gmail.com>
2022-03-07 13:38:29 -0800

    Add foreign key and one to one relations, create Restaurant and UserProfile data models

--- backend/dinesmart/models.py
+++ backend/dinesmart/models.py
@@ -2,12 +2,31 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
-class Users(models.Model):
+class User(models.Model):
     email = models.CharField(max_length=50, primary_key=True)
     password = models.CharField(max_length=200)
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    location = models.CharField(max_length=50)
+    num_bookings = models.IntegerField(default=0)
+    member_since = models.CharField(default='xx', max_length=50)
+
+class Restaurant(models.Model):
+    location = models.CharField(max_length=50)
+    cuisine = models.CharField(max_length=50)
+    average_rating = models.DecimalField
+    num_reviews = models.IntegerField
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant)
+    rating = models.IntegerField(validators=[MinValueValidator(0),
+                                       MaxValueValidator(5)])
+    content = models.CharField(max_length=500)
+
 class UserAuthTokens(models.Model):
-    email = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=200)
     timestamp = models.CharField(max_length=50)
 
@@ -15,10 +34,3 @@ class PasswordReset(models.Model):
     email = models.CharField(max_length=50)
     resetToken = models.CharField(max_length=50)
     timestamp = models.CharField(max_length=50)
-
-class Reviews(models.Model):
-    user = models.CharField(max_length=50)
-    restaurant = models.CharField(max_length=50)
-    rating = models.IntegerField(validators=[MinValueValidator(0),
-                                       MaxValueValidator(5)])
-    content = models.CharField(max_length=500)
