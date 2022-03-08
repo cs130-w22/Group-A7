0f577e978e933b979abbaecb03a5893cfd8e4dd6
Prithvi Kannan <46332835+prithvikannan@users.noreply.github.com>
2022-03-07 16:48:56 -0800

    getrestaurantbyid

--- frontend/src/Components/ReviewTile.js
+++ frontend/src/Components/ReviewTile.js
@@ -4,10 +4,34 @@ import { NavLink } from "react-router-dom";
 import axios from "axios";
 import Cookies from "js-cookie";
 export default class ReviewTile extends Component {
+  state = {
+    restaurant_name: ""
+  }
+  componentDidMount() {
+    var data = JSON.stringify({restaurant_id: this.state.restaurant_id})
+    axios({
+      method: "post",
+      url: "http://localhost:8000/getRestaurantById/",
+      headers: {
+        "Content-Type": "text/plain",
+        "X-CSRFToken": Cookies.get("XSRF-TOKEN"),
+      },
+      data: data,
+      withCredentials: "false",
+    })
+      .then((response) => {
+        if (response.data !== "") {
+          this.setState({ restaurant_name: response.data });
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
   render() {
     return (
       <Container>
-        <h2>Restaurant id: {this.props.review[1].restaurant_id}</h2>
+        <h2>Restaurant id: {this.state.restaurant_name}</h2>
         <h2>Written by: {this.props.review[1].user_id} on {this.props.review[1].timestamp}</h2>
         <h2>Stars: {this.props.review[1].rating}</h2>
         <h2>Review: {this.props.review[1].content}</h2>
