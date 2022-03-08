8efc3c4f5d032993192b2ec2fc0b9f9760ba73e3
Prithvi Kannan <46332835+prithvikannan@users.noreply.github.com>
2022-03-07 16:45:40 -0800

    update tile params

--- frontend/src/Components/ReviewTile.js
+++ frontend/src/Components/ReviewTile.js
@@ -7,10 +7,10 @@ export default class ReviewTile extends Component {
   render() {
     return (
       <Container>
-        <h1>{this.props.review.restaurant}</h1>
-        <h1>{this.props.review.author}</h1>
-        <h1>{this.props.review.rating}</h1>
-        <h1>{this.props.review.content}</h1>
+        <h2>Restaurant id: {this.props.review[1].restaurant_id}</h2>
+        <h2>Written by: {this.props.review[1].user_id} on {this.props.review[1].timestamp}</h2>
+        <h2>Stars: {this.props.review[1].rating}</h2>
+        <h2>Review: {this.props.review[1].content}</h2>
       </Container>
     );
   }
